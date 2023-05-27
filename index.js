53234adb72c3017231683c519031d53308591d81
Herit Viroja <105600353+heritviroja@users.noreply.github.com>
2023-05-27 21:01:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu");
+
+const sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
